1ab199383857260ef6744f61d95680677fa2a37f
Yogesh Naik <yogeshrnaik@gmail.com>
2015-11-08 15:40:44 +0530

    Code formatting

--- SpringMVCHibernate/src/main/java/com/journaldev/spring/controller/PersonController.java
+++ SpringMVCHibernate/src/main/java/com/journaldev/spring/controller/PersonController.java
@@ -13,45 +13,43 @@ import com.journaldev.spring.service.PersonService;
 
 @Controller
 public class PersonController {
-	
-	@Autowired(required=true)
+
+	@Autowired(required = true)
 	private PersonService personService;
-	
+
 	@RequestMapping(value = "/persons", method = RequestMethod.GET)
 	public String listPersons(Model model) {
 		model.addAttribute("person", new Person());
 		model.addAttribute("listPersons", this.personService.listPersons());
 		return "person";
 	}
-	
-	//For add and update person both
-	@RequestMapping(value= "/person/add", method = RequestMethod.POST)
-	public String addPerson(@ModelAttribute("person") Person p){
-		
-		if(p.getId() == 0){
-			//new person, add it
+
+	// For add and update person both
+	@RequestMapping(value = "/person/add", method = RequestMethod.POST)
+	public String addPerson(@ModelAttribute("person") Person p) {
+
+		if (p.getId() == 0) {
+			// new person, add it
 			this.personService.addPerson(p);
-		}else{
-			//existing person, call update
+		} else {
+			// existing person, call update
 			this.personService.updatePerson(p);
 		}
-		
+
 		return "redirect:/persons";
-		
+
 	}
-	
+
 	@RequestMapping("/remove/{id}")
-    public String removePerson(@PathVariable("id") int id){
-		
-        this.personService.removePerson(id);
-        return "redirect:/persons";
-    }
- 
-    @RequestMapping("/edit/{id}")
-    public String editPerson(@PathVariable("id") int id, Model model){
-        model.addAttribute("person", this.personService.getPersonById(id));
-        model.addAttribute("listPersons", this.personService.listPersons());
-        return "person";
-    }
-	
+	public String removePerson(@PathVariable("id") int id) {
+		this.personService.removePerson(id);
+		return "redirect:/persons";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public String editPerson(@PathVariable("id") int id, Model model) {
+		model.addAttribute("person", this.personService.getPersonById(id));
+		model.addAttribute("listPersons", this.personService.listPersons());
+		return "person";
+	}
 }

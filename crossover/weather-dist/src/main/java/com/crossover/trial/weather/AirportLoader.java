2ab9b84ca5f0e044cfcb43e7991d58e74748cd58
Yogesh Naik <yogeshrnaik@gmail.com>
2015-11-29 12:55:23 +0530

    removed un-necessary imports

--- crossover/weather-dist/src/main/java/com/crossover/trial/weather/AirportLoader.java
+++ crossover/weather-dist/src/main/java/com/crossover/trial/weather/AirportLoader.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 

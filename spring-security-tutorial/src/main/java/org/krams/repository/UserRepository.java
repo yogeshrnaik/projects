a079616654b3613e364986c8486483267c42b770
Yogesh Naik <yogeshnaik@gmail.com>
2019-03-24 14:04:04 +0530

    removed sample Spring MVC Hibernate and Spring security related projects

--- spring-security-tutorial/src/main/java/org/krams/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.krams.repository;
-
-import org.krams.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-	User findByUsername(String username);
-}
